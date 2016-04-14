de2527b9b1f465e60039a814da82172259f3d883
kam6512_rainbow <kam6512@gmail.com>
2016-04-14 12:21:34 +0900

    Update Gatt Logic Code in DeviceProfileActivity
    DeviceProfileActivity :
        - GattCustomCallbacks 인터페이스 추가 / GattCallbacks 객체 삭제, 코드 이전
        - deviceRSSI 변수 초기 값 수정
        - CoordinatorLayout 뷰 변수 추가
        - TAG_SERVICE_FRAGMENT
          TAG_CHARACTERISTIC_FRAGMENT
          TAG_CONTROL_FRAGMENT
          프래그먼트 태그 상수 추가
        - deviceDisconnect Runnable 변수 람다식으로 수정
        - Gatt 실패 콜백마다 SnackBar 로 메시지 전달

--- app/src/main/java/com/rainbow/kam/android_ble_control/ui/activity/DeviceProfileActivity.java
+++ app/src/main/java/com/rainbow/kam/android_ble_control/ui/activity/DeviceProfileActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +17,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.rainbow.kam.android_ble_control.R;
 import com.rainbow.kam.android_ble_control.data.adapter.CharacteristicAdapter;
@@ -33,6 +34,7 @@ import com.rainbow.kam.ble_gatt_manager.GattManager;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
+import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.res.StringRes;
 
@@ -43,15 +45,13 @@ import java.util.List;
  * Created by kam6512 on 2015-11-27.
  */
 @EActivity(R.layout.a_profile)
-public class DeviceProfileActivity extends AppCompatActivity
-        implements
+public class DeviceProfileActivity extends AppCompatActivity implements
         ServiceListFragment.OnServiceReadyListener,
         CharacteristicListFragment.OnCharacteristicReadyListener,
-
         ServiceAdapter.OnServiceItemClickListener,
         CharacteristicAdapter.OnCharacteristicItemClickListener,
-
-        ControlFragment.OnControlListener {
+        ControlFragment.OnControlListener,
+        GattCustomCallbacks {
 
     private final String TAG = getClass().getSimpleName();
 
@@ -63,13 +63,15 @@ public class DeviceProfileActivity extends AppCompatActivity
 
     @Extra(ScanActivity.KEY_DEVICE_NAME) String deviceName;
     @Extra(ScanActivity.KEY_DEVICE_ADDRESS) String deviceAddress;
-    private String deviceRSSI = "- - " + RSSI_UNIT;
+    private String deviceRSSI;
 
 
     @ViewById(R.id.profile_toolbar) Toolbar toolbar;
     @ViewById(R.id.profile_name) TextView deviceNameTextView;
     @ViewById(R.id.profile_address) TextView deviceAddressTextView;
     @ViewById(R.id.profile_rssi) TextView deviceRSSITextView;
+    @ViewById(R.id.profile_root) CoordinatorLayout rootLayout;
+
     @ViewById(R.id.profile_state) TextView deviceStateTextView;
 
     private FragmentManager fragmentManager;
@@ -78,6 +80,10 @@ public class DeviceProfileActivity extends AppCompatActivity
     private CharacteristicListFragment_ characteristicListFragment;
     private ControlFragment_ controlFragment;
 
+    private static final String TAG_SERVICE_FRAGMENT = "SERVICE";
+    private static final String TAG_CHARACTERISTIC_FRAGMENT = "CHARACTERISTIC";
+    private static final String TAG_CONTROL_FRAGMENT = "CONTROL";
+
     private GattManager gattManager;
 
     private List<BluetoothGattService> bluetoothGattServices;
@@ -85,12 +91,9 @@ public class DeviceProfileActivity extends AppCompatActivity
 
     private BluetoothGattCharacteristic controlCharacteristic;
 
-    private final Runnable deviceDisconnect = new Runnable() {
-        @Override
-        public void run() {
-            deviceStateTextView.setText(disconnectedLabel);
-            new Handler().postDelayed(DeviceProfileActivity.this::finish, 500);
-        }
+    private final Runnable deviceDisconnect = () -> {
+        deviceStateTextView.setText(disconnectedLabel);
+        new Handler().postDelayed(DeviceProfileActivity.this::finish, 500);
     };
 
 
@@ -103,6 +106,7 @@ public class DeviceProfileActivity extends AppCompatActivity
     @AfterViews void setToolbar() {
         deviceNameTextView.setText(deviceName);
         deviceAddressTextView.setText(deviceAddress);
+        deviceRSSI = "-" + RSSI_UNIT;
         deviceRSSITextView.setText(deviceRSSI);
 
         setSupportActionBar(toolbar);
@@ -114,7 +118,7 @@ public class DeviceProfileActivity extends AppCompatActivity
     }
 
 
-     void setFragments() {
+    private void setFragments() {
         fragmentManager = getSupportFragmentManager();
 
         serviceListFragment = new ServiceListFragment_();
@@ -125,63 +129,8 @@ public class DeviceProfileActivity extends AppCompatActivity
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        registerBluetooth();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (gattManager.isConnected()) {
-            disconnectDevice();
-        } else {
-            finish();
-        }
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        BluetoothHelper.onRequestEnableResult(requestCode, resultCode, this);
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        BluetoothHelper.onRequestPermissionsResult(requestCode, grantResults, this);
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        if (gattManager.isConnected()) {
-            if (serviceListFragment.isVisible()) {
-                disconnectDevice();
-            } else {
-                super.onBackPressed();
-            }
-        } else {
-            finish();
-        }
-    }
-
-
     private void registerBluetooth() {
-        gattManager = new GattManager(this, gattCallbacks);
+        gattManager = new GattManager(getApplication(), this);
         if (gattManager.isBluetoothAvailable()) {
             connectDevice();
         } else {
@@ -213,136 +162,189 @@ public class DeviceProfileActivity extends AppCompatActivity
     }
 
 
-    private final GattCustomCallbacks.GattCallbacks gattCallbacks = new GattCustomCallbacks.GattCallbacks() {
-        public void onDeviceConnected() {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    deviceStateTextView.setText(connectedLabel);
-                }
-            });
+    @Override protected void onResume() {
+        super.onResume();
+        registerBluetooth();
+    }
+
+
+    @Override protected void onPause() {
+        super.onPause();
+        if (gattManager.isConnected()) {
+            disconnectDevice();
+        } else {
+            finish();
         }
+    }
 
 
-        public void onDeviceDisconnected() {
-            runOnUiThread(deviceDisconnect);
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
+    }
 
 
-        @Override public void onServicesFound(BluetoothGatt bluetoothGatt) {
-            bluetoothGattServices = bluetoothGatt.getServices();
-            onServiceReady();
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        BluetoothHelper.onRequestEnableResult(requestCode, resultCode, this);
+    }
+
+
+    @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                                     @NonNull int[] grantResults) {
+        BluetoothHelper.onRequestPermissionsResult(requestCode, grantResults, this);
+    }
+
+
+    @Override public void onBackPressed() {
+        if (gattManager.isConnected()) {
+            if (serviceListFragment.isVisible()) {
+                disconnectDevice();
+            } else {
+                super.onBackPressed();
+            }
+        } else {
+            finish();
         }
+    }
 
 
-        public void onReadSuccess(final BluetoothGattCharacteristic ch) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    controlFragment.newValueForCharacteristic(ch);
-                }
-            });
-        }
+    @UiThread @Override public void onDeviceConnected() {
+        deviceStateTextView.setText(connectedLabel);
+    }
 
 
-        public void onReadFail() {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    controlFragment.setFail();
-
-                }
-            });
-        }
+    @Override public void onDeviceConnectFail(Exception e) {
+        showExceptionMessage(e.getMessage());
+    }
 
 
-        public void onDeviceNotify(final BluetoothGattCharacteristic ch) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    controlFragment.newValueForCharacteristic(ch);
-
-                }
-            });
-        }
+    @UiThread @Override public void onDeviceDisconnected() {
+        deviceDisconnect.run();
+    }
 
 
-        public void onWriteSuccess() {
-            Toast.makeText(DeviceProfileActivity.this, "onWriteSuccess", Toast.LENGTH_SHORT).show();
-        }
+    @Override public void onDeviceDisconnectFail(Exception e) {
+        showExceptionMessage(e.getMessage());
+    }
 
 
-        public void onWriteFail() {
-            Toast.makeText(DeviceProfileActivity.this, "onWriteFail", Toast.LENGTH_SHORT).show();
-        }
+    @UiThread @Override public void onServicesFound(BluetoothGatt bluetoothGatt) {
+        bluetoothGattServices = bluetoothGatt.getServices();
+        onServiceReady();
+    }
 
 
-        public void onRSSIUpdate(final int rssi) {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    deviceRSSI = rssi + RSSI_UNIT;
-                    deviceRSSITextView.setText(deviceRSSI);
-                }
-            });
-        }
-    };
+    @Override public void onServicesNotFound(Exception e) {
+        showExceptionMessage(e.getMessage());
+    }
 
 
-    @Override
-    public void onServiceItemClick(int position) {
-        fragmentManager.beginTransaction().addToBackStack("characteristic").replace(R.id.profile_fragment_view, characteristicListFragment).commit();
+    @Override public void onDeviceReady() {
+        Snackbar.make(rootLayout, "onDeviceReady", Snackbar.LENGTH_LONG).show();
+    }
+
+
+    @UiThread @Override public void onReadSuccess(final BluetoothGattCharacteristic ch) {
+        controlFragment.newValueForCharacteristic(ch);
+    }
+
+
+    @UiThread @Override public void onReadFail(Exception e) {
+        controlFragment.setFail();
+        showExceptionMessage(e.getMessage());
+    }
+
+
+    @UiThread @Override public void onDeviceNotify(final BluetoothGattCharacteristic ch) {
+        controlFragment.newValueForCharacteristic(ch);
+    }
+
+
+    @Override public void onSetNotificationSuccess() {
+        Snackbar.make(rootLayout, "onSetNotificationSuccess", Snackbar.LENGTH_LONG).show();
+    }
+
+
+    @Override public void onSetNotificationFail(Exception e) {
+        showExceptionMessage(e.getMessage());
+    }
+
+
+    @Override public void onWriteSuccess() {
+        Snackbar.make(rootLayout, "onWriteSuccess", Snackbar.LENGTH_LONG).show();
+    }
+
+
+    @Override public void onWriteFail(Exception e) {
+        showExceptionMessage(e.getMessage());
+    }
+
+
+    @UiThread @Override public void onRSSIUpdate(final int rssi) {
+        deviceRSSI = rssi + RSSI_UNIT;
+        deviceRSSITextView.setText(deviceRSSI);
+    }
+
+
+    @UiThread void showExceptionMessage(String msg) {
+        Snackbar.make(rootLayout, msg, Snackbar.LENGTH_LONG).show();
+    }
+
+
+    @Override public void onRSSIMiss() {
+        Snackbar.make(rootLayout, "onRSSIMiss", Snackbar.LENGTH_LONG).show();
+    }
+
+
+    @Override public void onServiceItemClick(int position) {
+        fragmentManager.beginTransaction().addToBackStack(TAG_CHARACTERISTIC_FRAGMENT).replace(R.id.profile_fragment_view, characteristicListFragment).commit();
         bluetoothGattCharacteristics = bluetoothGattServices.get(position).getCharacteristics();
         onCharacteristicReady();
 
     }
 
 
-    @Override
-    public void onCharacteristicItemClick(int position) {
-        fragmentManager.beginTransaction().addToBackStack("control").replace(R.id.profile_fragment_view, controlFragment).commit();
+    @Override public void onCharacteristicItemClick(int position) {
+        fragmentManager.beginTransaction().addToBackStack(TAG_CONTROL_FRAGMENT).replace(R.id.profile_fragment_view, controlFragment).commit();
         if (!bluetoothGattCharacteristics.get(position).equals(controlCharacteristic)) {
             controlCharacteristic = bluetoothGattCharacteristics.get(position);
         }
     }
 
 
-    @Override
-    public void onServiceReady() {
+    @Override public void onServiceReady() {
         if (serviceListFragment.isVisible() && bluetoothGattServices != null) {
             runOnUiThread(() -> serviceListFragment.setServiceList(bluetoothGattServices));
         }
     }
 
 
-    @Override
-    public void onCharacteristicReady() {
+    @Override public void onCharacteristicReady() {
         if (characteristicListFragment.isVisible() && bluetoothGattServices != null) {
             runOnUiThread(() -> characteristicListFragment.setCharacteristicList(bluetoothGattCharacteristics));
         }
     }
 
 
-    @Override
-    public void onControlReady() {
+    @Override public void onControlReady() {
         controlFragment.init(deviceName, deviceAddress, controlCharacteristic);
     }
 
 
-    @Override
-    public void setNotification(boolean isNotificationEnable) {
+    @Override public void setNotification(boolean isNotificationEnable) {
         gattManager.setNotification(controlCharacteristic, isNotificationEnable);
     }
 
 
-    @Override
-    public void setReadValue() {
+    @Override public void setReadValue() {
         gattManager.readValue(controlCharacteristic);
     }
 
 
-    @Override
-    public void setWriteValue(byte[] data) {
+    @Override public void setWriteValue(byte[] data) {
         gattManager.writeValue(controlCharacteristic, data);
     }
 }
