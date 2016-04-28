7ef5dc66c4481a4918729ea4bf57c3fb5be067f0
kam6512_rainbow <kam6512@gmail.com>
2016-04-28 12:08:46 +0900

    리스너 인터페이스 제거

--- app/src/main/java/com/rainbow/kam/android_ble_control/listener/async/ViewGattAsyncListener.java
+++
@@ -1,7 +0,0 @@
-package com.rainbow.kam.android_ble_control.listener.async;
-
-/**
- * Created by kam6512 on 2016-04-19.
- */
-public interface ViewGattAsyncListener {
-}
--- app/src/main/java/com/rainbow/kam/android_ble_control/listener/click/OnCharacteristicItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.rainbow.kam.android_ble_control.listener.click;
-
-/**
- * Created by kam6512 on 2016-04-19.
- */
-public interface OnCharacteristicItemClickListener {
-    void onCharacteristicItemClick(int position);
-}
