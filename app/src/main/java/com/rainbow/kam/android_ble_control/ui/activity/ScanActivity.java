b01320973088653f4c3f0aec9f80566d767977a5
kam6512_rainbow <kam6512@gmail.com>
2016-05-18 12:14:08 +0900

    스캔 Rx 코드 수정

--- app/src/main/java/com/rainbow/kam/android_ble_control/ui/activity/ScanActivity.java
+++ app/src/main/java/com/rainbow/kam/android_ble_control/ui/activity/ScanActivity.java
@@ -2,12 +2,16 @@ package com.rainbow.kam.android_ble_control.ui.activity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import com.github.pwittchen.reactivebeacons.library.Beacon;
 import com.github.pwittchen.reactivebeacons.library.ReactiveBeacons;
 import com.rainbow.kam.android_ble_control.BuildConfig;
 import com.rainbow.kam.android_ble_control.R;
@@ -28,6 +32,8 @@ import javax.inject.Inject;
 import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
+import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
 /**
@@ -36,8 +42,7 @@ import rx.schedulers.Schedulers;
 @SuppressLint("Registered")
 @EActivity(R.layout.a_scan)
 public class ScanActivity extends BaseActivity implements
-        SwipeRefreshLayout.OnRefreshListener,
-        OnDeviceSelectListener {
+        OnRefreshListener, OnDeviceSelectListener {
 
 
     @ViewById(R.id.scan_root) SwipeRefreshLayout refreshScanLayout;
@@ -53,6 +58,11 @@ public class ScanActivity extends BaseActivity implements
 
     @Override protected void injectComponent(ActivityComponent component) {
         component.inject(this);
+    }
+
+
+    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         reactiveBeacons = new ReactiveBeacons(this);
     }
 
@@ -130,25 +140,15 @@ public class ScanActivity extends BaseActivity implements
 
 
     private void startScan() {
-        if (canObserveBeacons()) {
-            beaconSubscription = reactiveBeacons.observe()
-                    .take(5, TimeUnit.SECONDS)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .doOnNext(beacon -> {
-                        if (!refreshScanLayout.isRefreshing()) {
-                            refreshScanLayout.setRefreshing(true);
-                        }
-                    })
-                    .doOnUnsubscribe(() -> {
-                        refreshScanLayout.setRefreshing(false);
-                        if (deviceAdapter.getItemCount() == 0) {
-                            scanEmptySnackBar.show();
-                        }
-                    })
-                    .map(beacon1 -> new DeviceItem(beacon1.device, beacon1.rssi))
-                    .subscribe(deviceAdapter::addDevice);
-        }
+        beaconSubscription = reactiveBeacons.observe()
+                .take(5, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(canObserveBeacons())
+                .doOnUnsubscribe(endScan())
+                .doOnNext(startRefresh())
+                .map(beacon1 -> new DeviceItem(beacon1.device, beacon1.rssi))
+                .subscribe(deviceAdapter::addDevice);
     }
 
 
@@ -160,17 +160,37 @@ public class ScanActivity extends BaseActivity implements
     }
 
 
-    private boolean canObserveBeacons() {
-        if (!reactiveBeacons.isBleSupported()) {
-            Snackbar.make(refreshScanLayout, R.string.bluetooth_fail, Snackbar.LENGTH_SHORT).show();
-            return false;
-        }
+    private Action1<Beacon> startRefresh() {
+        return beacon -> {
+            if (!refreshScanLayout.isRefreshing()) {
+                refreshScanLayout.setRefreshing(true);
+            }
+        };
+    }
 
-        if (!reactiveBeacons.isBluetoothEnabled()) {
-            reactiveBeacons.requestBluetoothAccess(this);
-            return false;
-        }
-        BluetoothHelper.requestBluetoothPermission(this);
-        return true;
+
+    private Action0 endScan() {
+        return () -> {
+            refreshScanLayout.setRefreshing(false);
+            if (deviceAdapter.getItemCount() == 0) {
+                scanEmptySnackBar.show();
+            }
+        };
+    }
+
+
+    private Action0 canObserveBeacons() {
+        return () -> {
+            if (!reactiveBeacons.isBleSupported()) {
+                Snackbar.make(refreshScanLayout, R.string.bluetooth_fail, Snackbar.LENGTH_SHORT).show();
+                beaconSubscription.unsubscribe();
+            }
+
+            if (!reactiveBeacons.isBluetoothEnabled()) {
+                reactiveBeacons.requestBluetoothAccess(ScanActivity.this);
+                beaconSubscription.unsubscribe();
+            }
+            BluetoothHelper.requestBluetoothPermission(ScanActivity.this);
+        };
     }
 }
